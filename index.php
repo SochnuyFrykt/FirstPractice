3341b6a5a8ceaf941d21eaa336c8a2cc40953468
SochnuyFrykt <serebrenikovdanil@gmail.com>
2023-01-30 20:32:24 +0400

    Added table

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <!doctype html>
 <html lang="`en`">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CRUD</title>
-    <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CRUD</title>
+  <link rel="stylesheet" type="text/css" href="styles/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
+  <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 </head>
 <body>
-    <div class="conteiner">
-        <div class="row">
-            <div class="col-12">
-
-            </div>
-        </div>
+  <div class="conteiner px-5 py-3">
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-primary">Добавить <i class="fa-solid fa-plus"></i></button>
+        <table class="table table-secondary border-light table-bordered my-4">
+          <thead>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>aaa</td>
+              <td>aaa</td>
+              <td>aaa</td>
+              <td>
+                <a href="" class="btn btn-success"><i class="fa-solid fa-edit"></i></a>
+                <a href="" class="btn btn-danger"><i class="fa-solid fa-trash-alt"></i></a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+   </div>
 </body>
 </html>
