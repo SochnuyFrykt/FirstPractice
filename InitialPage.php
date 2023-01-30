fe39172f7fa40afb0af042532e7503e55849e6fc
SochnuyFrykt <serebrenikovdanil@gmail.com>
2023-01-30 19:04:09 +0400

    Creating a welcome page

---
+++ InitialPage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Hello, User</title>
+  <link rel="stylesheet" type="text/css" href="styles/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
+</head>
+<body>
+
+<div class="message">
+  <div class="message-box">
+    Для использования нашего сервиса, вам нужно
+    <button href="" type="button" class="btn btn-primary">Зарегистрироваться</button> и
+    <button href="../php/Login.php" type="button" class="btn btn-outline-primary">Войти</button> в свою учётную запись.
+  </div>
+</div>
+
+</body>
+</html>
